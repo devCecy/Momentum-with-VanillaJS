5566a2d0a38fc5e84839897ae17bbf23157c014e
devCecy <im.ceciliaan@gmail.com>
2020-11-09 00:24:28 +0900

    toDoList 2nd try

--- toDoList.js
+++ toDoList.js
@@ -1,34 +1,47 @@
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector("js-toDoList");
+const toDoList = document.querySelector(".js-toDoList");
 
-const toDoLocal = "toDoKey"
+const toDoLocal = "toDoKey";
+const toDoArr = [];
 
 function toDoSave (text){
-    localStorage.setItem(toDoLocal, text);
+    localStorage.setItem(toDoLocal, JSON.stringify(toDoArr));
+}
+
+function printToDo (text){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = `${text}`;
+    li.appendChild(span);
+    toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    
 }
 
 function toDoHandler (event){
     event.preventDefault();
     const toDoInputValue = toDoInput.value;
     printToDo(toDoInputValue);
-    toDoSave(toDoInputValue);
-}
-
-function printToDo (text){
-    toDoForm.addEventListener("submit", toDoHandler);
-    toDoInput.innerText = `${text}`;
 }
 
 function loadToDo (){
     const toDoKey = localStorage.getItem(toDoLocal);
     if (toDoKey !== null){
-        printToDo(toDoKey);
-    }
+        const parsedToDos = JSON.parse(toDoKey);
+        parsedToDos.forEach(function(toDo){
+        printToDo(toDo.text);
+    });
+}
 } 
 
 function init(){
     loadToDo();
+    toDoForm.addEventListener("submit", toDoHandler);
 }
 
 init();
