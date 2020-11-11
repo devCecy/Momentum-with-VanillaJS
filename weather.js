492d5429e014663720face3cad744cb17dd6c3f8
devCecy <im.ceciliaan@gmail.com>
2020-11-11 12:36:45 +0900

    finished get weather

--- weather.js
+++ weather.js
@@ -1,7 +1,23 @@
+const weather = document.querySelector(".js-weather");
+const API_KRY = "4177782532afcd4eb994a9c987bd97d7";
 const COORDS = "coords";
 
-function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+function getWeather(lat,lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KRY}&units=metric`
+    ) // fetch를 통해 데이터를 가져 올 수 있다. 앞부분에 https://넣어줄것 //&units=metric 을 넣어 단위 맞춰줌!
+        .then(function(response){ // then은 데이터가 완전히 들어온 다음에 호출한다. 
+            return response.json();
+        }) 
+        .then (function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @${place}`// html에 넣어주기 
+        });
+
+        function saveCoords(coordsObj){
+            localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+        }
 }
 
 function handleGeoSuccess(position){
@@ -12,6 +28,7 @@ function handleGeoSuccess(position){
        longitude//longitude:longitude     
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError(){
@@ -27,7 +44,8 @@ function loadCoords(){
     if (loadedCoords === null){
         askForCoords();
     } else {
-        //get weahter
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
